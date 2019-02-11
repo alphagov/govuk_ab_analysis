a013bea109aff25892e824009dc044696c5237f7
Suganya Sivaskantharajah <suganya.sivaskantharajah@digital.cabinet-office.gov.uk>
2019-02-11 17:38:47 +0000

    WIP sampling and rolling up data - needs testing

--- src/sample_processed.py
+++ src/sample_processed.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import random
-#.. other safe imports
+# .. other safe imports
 try:
     import pandas as pd
     import numpy as np
@@ -13,12 +13,19 @@ except ImportError:
 
 print("other modules loaded")
 
-REQUIRED_COLUMNS = ["Occurrences", "ABVariant", "Sequence", 'Page_Event_List',
-                    'Page_List', 'PageSequence', 'Event_List',
-                    'num_event_cats', 'Event_cats_agg', 'Event_cat_act_agg']
+REQUIRED_COLUMNS = ["Occurrences", "ABVariant", 'Page_Event_List',
+                    "Page_List",  "Event_cat_act_agg"
+                    # ,"Sequence", 'PageSequence', 'Event_List',
+                    # 'num_event_cats', "Event_cats_agg"
+                    ]
 
 DATA_DIR = os.getenv("DATA_DIR")
 print(DATA_DIR)
+
+
+def is_a_b(variant):
+    return any([variant == x for x in ['A', 'B']])
+
 
 def sample_processed_journey(filename, seed=1337, k=1000, with_replacement=True):
     """
@@ -40,25 +47,19 @@ def sample_processed_journey(filename, seed=1337, k=1000, with_replacement=True)
 
     # having issue with global env, $PWD not recognised in pycharm, so can't use DATA_DIR
     # assume current work dir is project dir
-    path = os.path.join(os.getcwd(), "data", "processed_journey", filename)
+    in_path = os.path.join(DATA_DIR, "processed_journey", filename)
     # the above adds slashes, need separate for file type
-    path = path + ".csv.gz"
+    in_path = in_path + ".csv.gz"
 
     print("Reading in file...")
 
-    df = pd.read_csv(path, sep='\t')
+    df = pd.read_csv(in_path, sep='\t', usecols=REQUIRED_COLUMNS)
 
-    print("Finished reading, now sampling...")
+    print("Finished reading, now removing any non A or B variants")
 
-    # create list of row indexes, a bag to draw from
-    elements = list(df.index.values)
-
-    # need weights as probabilities, should sum to one, more occurrences more probable
-    total_occur = sum(df.Occurrences)
-    weights = df.Occurrences / total_occur
-
-    # reproducible
-    random.seed(seed)
+    # filter out any values like Object object
+    df['is_a_b'] = df['ABVariant'].map(is_a_b)
+    df = df[df['is_a_b']]
 
     # size should be an arg, what is the desired sample size we are after?
     # this will need to consider each day, and what day of the week it is
@@ -66,20 +67,35 @@ def sample_processed_journey(filename, seed=1337, k=1000, with_replacement=True)
     # as an mvp the user could provide a list of proportions for each filename
     # or we could expect to get 7 filenames, and then hard code the sizes
     # to abstract away this complexity
-    our_sample = choice(a=elements, size=k, p=weights, replace=with_replacement)
+    # select rows at random
+    df_sampled = df.sample(n=k, replace=with_replacement,
+                           weights=df.Occurrences, random_state=seed)
 
-    # select rows and set occurrences to one
-    df_sampled = df.iloc[our_sample]
-
-    # set all rows of Occurrences col to one
+    # instead of setting all rows of Occurrences col to one, we can just group
+    # by the other things and count the rows instead of summing
+    # occurrences, to roll up data
     # df_sampled.loc[:, "Occurrences"] = 1
-    df_sampled.assign(Occurrences=1)
+    # df_sampled = df_sampled.assign(Occurrences=1)
+    # df_sampled['Occurrences'] = 1
 
     # return something to show it's worked
     print(df_sampled.info())
 
+    print("rolling up data")
+    cols_without_occurrences = REQUIRED_COLUMNS.copy()
+    cols_without_occurrences.remove('Occurrences')
+    df_sampled_grouped = df_sampled.groupby(
+        cols_without_occurrences).count().reset_index()
+
+    print(df_sampled_grouped.info())
+
     print("Saving to data/sampled_journey")
+
+    out_path = os.path.join(DATA_DIR, "sampled_journey", filename)
+    out_path = out_path + ".csv.gz"
+    df_sampled_grouped.to_csv(out_path, sep='\t', compression='gzip')
     # slow and too big, need to roll up
+
     return None
 
 
