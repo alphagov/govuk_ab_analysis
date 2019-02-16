6edf8b21c2f9012f13b0c52aac79b7caecfd56c7
Matthew Gregory <matthew.gregory@digital.cabinet-office.gov.uk>
2019-02-16 22:16:57 +0000

    Move metadata assignment, to fix scoping

--- src/analysis.py
+++ src/analysis.py
@@ -64,6 +64,48 @@ def is_related(x):
     return x > 0
 
 
+def is_nav_event(event):
+    """
+    Return the total number of related links clicks for that row.
+
+    Clicks per sequence multiplied by occurrences.
+    """
+    return any(
+        ['breadcrumbClicked' in event, 'homeLinkClicked' in event,
+         all(cond in event for cond in [
+             'relatedLinkClicked', 'Explore the topic'])])
+
+
+def count_nav_events(page_event_list):
+    """
+    Counts the number of nav events from a content page in a Page Event List.
+
+    Helper function dependent on thing_page_paths instantiated in analyse_sampled_processed_journey.
+
+    """
+    content_page_nav_events = 0
+    for pair in page_event_list:
+        if is_nav_event(pair[1]):
+            if pair[0] in thing_page_paths:
+                content_page_nav_events += 1
+    return content_page_nav_events
+
+
+def count_search_from_content(page_list):
+    """
+    Counts the number of GOV.UK searches in the given journey.
+
+    Helper function dependent on thing_page_paths instantiated in analyse_sampled_processed_journey.
+    """
+    search_from_content = 0
+    for i, page in enumerate(page_list):
+        if i > 0:
+            if '/search?q=' in page:
+                if page_list[i-1] in thing_page_paths:
+                    search_from_content += 1
+    return search_from_content
+
+
 def compute_standard_error_prop_two_samples(x_a, n_a, x_b, n_b):
     """
     The standard error of the difference between two proportions is given by the square root of the variances.
@@ -141,31 +183,38 @@ def analyse_sampled_processed_journey(data_dir, filename):
 
     df['Page_List_Length'] = df['Page_List'].progress_apply(len)
 
-    logger.info('Reading in Page metadata...')
-
-    metadata_path = os.path.join(
-        data_dir, 'metadata',
-        'document_types.csv.gz')
-
-    df_finding_thing = pd.read_csv(metadata_path, sep="\t", compression="gzip")
-
-    logger.info('Creating thing and finding thing lists...')
-
-    thing_page_paths = df_finding_thing[
-        df_finding_thing['is_finding'] == 0]['pagePath'].tolist()
-
-    finding_page_paths = df_finding_thing[
-        df_finding_thing['is_finding'] == 1]['pagePath'].tolist()
-
     logger.info('Related link preparation...')
     logger.debug('Get the number of related links clicks per Sequence')
     df['Related Links Clicks per seq'] = df['Event_cat_act_agg'].progress_map(sum_related_click_events)
-    df["Has_Related"] = df["Related Links Clicks per seq"].map(is_related)
+    logger.debug('Calculate number of related links per experimental unit.')
+    df["Has_Related"] = df["Related Links Clicks per seq"].progress_map(is_related)
     df['Related Links Clicks row total'] = df['Related Links Clicks per seq'] * df['Occurrences']
+
+    # needs finding_thing_df read in from document_types.csv.gz
+    logger.info('Navigation events preparation...')
+    df['Content_Page_Nav_Event_Count'] = df['Page_Event_List'].progress_map(count_nav_events)
+    logger.info('Search events preparation...')
+    df['Content_Search_Event_Count'] = df['Page_List'].progress_map(count_search_from_content)
+    logger.debug('Calculating the ratio of clicks on navigation elements vs. clicks on related links')
+    df['Ratio_Nav_Search_to_Rel'] = (df['Content_Page_Nav_Event_Count'] + df.Content_Search_Event_Count + 1) / (
+                df['Related Links Clicks row total'] + 1)
+
+    logger.info('All necessary variables derived for pending statistical tests...')
+
+    df_ab = pd.DataFrame(columns=['metric_name', 'stats_method',
+                             'x_ab', 'n_ab', 'p_ab',
+                             'x_a', 'n_a', 'p_a',
+                             'x_b', 'n_b', 'p_b',
+                             'test_statistic', 'uci_dif', 'lci_dif',
+                             'alpha', 'h0_status', 'relative_uplift'])
+
+    logger.info(df_ab)
+
+    logger.info('Saving csv to reports dir')
 
     out_path = os.path.join(REPORTS_DIR, f"{filename}")
     logger.info(f"Saving to {out_path}")
-    # df_ab.to_csv(out_path, sep="\t", compression="gzip", index=False)
+    df_ab.to_csv(out_path, sep="\t", compression="gzip", index=False)
 
     return None
 
@@ -194,6 +243,24 @@ if __name__ == "__main__":  # our module is being executed as a program
     DATA_DIR = os.getenv("DATA_DIR")
     REPORTS_DIR = os.getenv("REPORTS_DIR")
     logger.debug("data directory" + DATA_DIR)
+
+    logger.info('Reading in Page metadata...')
+
+    metadata_path = os.path.join(
+        DATA_DIR, 'metadata',
+        'document_types.csv.gz')
+
+    logger.debug(f'Reading in metadata from {metadata_path}')
+    df_finding_thing = pd.read_csv(metadata_path, sep="\t", compression="gzip")
+    logger.debug(print('metadata head:', df_finding_thing.head(3)))
+
+    logger.info('Creating thing and finding thing lists...')
+
+    thing_page_paths = df_finding_thing[
+        df_finding_thing['is_finding'] == 0]['pagePath'].tolist()
+
+    finding_page_paths = df_finding_thing[
+        df_finding_thing['is_finding'] == 1]['pagePath'].tolist()
 
     print()
     analyse_sampled_processed_journey(DATA_DIR, args.filename)
